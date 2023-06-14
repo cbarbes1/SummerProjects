1ead305fc3e4977edd9af93884bf3f4a658d0b99
cbarbes <cbarbes02@gmail.com>
2023-06-13 23:06:49 -0400

    Added files for test program creation.

---
+++ MapReduce/Program01/FrequencyMapper.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class FrequencyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
